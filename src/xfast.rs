7f08f97edad8ee1874e83fe361d1a9ec76890d25
Alan Du <alanhdu@gmail.com>
2019-04-12 22:56:24 -0400

    Implement remove

--- src/xfast.rs
+++ src/xfast.rs
@@ -12,7 +12,7 @@ fn upper_bits(key: u32, n: usize) -> u32 {
     key & (0xFFFF_FFFF << (31 - n))
 }
 
-impl<T> XFastMap<T> {
+impl<T: std::fmt::Debug> XFastMap<T> {
     pub fn new() -> XFastMap<T> {
         XFastMap {
             map: HashMap::new(),
@@ -32,12 +32,16 @@ impl<T> XFastMap<T> {
 
     /// Return a reference to the value corresponding to the key
     pub fn get(&self, key: u32) -> Option<&T> {
-        self.map.get(&key).map(|node| &unsafe { node.as_ref() }.value)
+        self.map
+            .get(&key)
+            .map(|node| &unsafe { node.as_ref() }.value)
     }
 
     /// Return a reference to the value corresponding to the key
     pub fn get_mut(&mut self, key: u32) -> Option<&mut T> {
-        self.map.get_mut(&key).map(|node| &mut unsafe { node.as_mut() }.value)
+        self.map
+            .get_mut(&key)
+            .map(|node| &mut unsafe { node.as_mut() }.value)
     }
 
     /// Return a reference to the value corresponding to the key
@@ -55,6 +59,26 @@ impl<T> XFastMap<T> {
                 let node = self.lss.insert(key, value);
                 v.insert(node);
                 None
+            }
+        }
+    }
+
+    pub fn remove(&mut self, key: u32) -> Option<T> {
+        match self.map.entry(key) {
+            Entry::Vacant(_) => None,
+            Entry::Occupied(o) => {
+                let mut ptr = o.remove();
+                let node = unsafe { Box::from_raw(ptr.as_mut()) };
+                self.lss.remove(&node);
+                unsafe {
+                    if let Some(prev) = node.prev.as_mut() {
+                        prev.next = node.next;
+                    }
+                    if let Some(next) = node.next.as_mut() {
+                        next.prev = node.prev;
+                    }
+                }
+                Some(node.value)
             }
         }
     }
@@ -101,7 +125,7 @@ struct LevelSearch<T> {
     root: Option<Descendant<T>>,
 }
 
-impl<T> LevelSearch<T> {
+impl<T: std::fmt::Debug> LevelSearch<T> {
     fn new() -> LevelSearch<T> {
         unsafe {
             let mut lss: LevelSearch<T> = LevelSearch {
@@ -115,6 +139,7 @@ impl<T> LevelSearch<T> {
         }
     }
 
+    /// Insert into the LevelSearch struct. Return pointer to the LNode
     fn insert(&mut self, key: u32, value: T) -> ptr::NonNull<LNode<u32, T>> {
         let node = unsafe {
             ptr::NonNull::new_unchecked(Box::into_raw(Box::new(LNode {
@@ -156,6 +181,73 @@ impl<T> LevelSearch<T> {
             self.root = Some(child);
         }
         node
+    }
+
+    /// Remove an LNode from the LSS
+    ///
+    /// Note that this assumes that `node ` is actually *in* the LSS and
+    /// will panic otherwise
+    fn remove(&mut self, node: &LNode<u32, T>) {
+        fn common_prefix_len(a: u32, b: u32) -> u32 {
+            (a ^ b).leading_zeros()
+        }
+        let max_prefix_len = unsafe {
+            match (node.next.as_ref(), node.prev.as_ref()) {
+                (None, None) => {
+                    self.root = None;
+                    self.maps.iter_mut().for_each(HashMap::clear);
+                    return;
+                }
+                (Some(next), None) => common_prefix_len(next.key, node.key),
+                (None, Some(prev)) => common_prefix_len(prev.key, node.key),
+                (Some(next), Some(prev)) => std::cmp::max(
+                    common_prefix_len(prev.key, node.key),
+                    common_prefix_len(next.key, node.key),
+                ),
+            }
+        } as usize;
+
+        let desc = if max_prefix_len == 0 {
+            self.root.as_mut().unwrap()
+        } else {
+            self.maps[max_prefix_len - 1]
+                .get_mut(&upper_bits(node.key, max_prefix_len - 1))
+                .unwrap()
+        };
+        match desc {
+            Descendant::Both => unsafe {
+                let high_bit = node.key & (1 << (31 - max_prefix_len));
+                *desc = if high_bit == 0 {
+                    debug_assert!(!node.next.is_null());
+                    Descendant::One {
+                        min: ptr::NonNull::new_unchecked(node.next),
+                    }
+                } else {
+                    debug_assert!(!node.prev.is_null());
+                    Descendant::Zero {
+                        max: ptr::NonNull::new_unchecked(node.prev),
+                    }
+                }
+            },
+            _ => {
+                debug_assert_eq!(max_prefix_len, 0);
+                self.root = None;
+            }
+        }
+
+        if let Some(ref mut desc) = self.root {
+            unsafe { desc.remove(node) };
+        }
+        for i in 0..max_prefix_len {
+            match self.maps[i].get_mut(&upper_bits(node.key, i)) {
+                Some(desc) => unsafe { desc.remove(node) },
+                None => unreachable!(),
+            }
+        }
+
+        for i in max_prefix_len..self.maps.len() {
+            self.maps[i].remove(&upper_bits(node.key, i));
+        }
     }
 
     fn longest_descendant(&self, key: u32) -> &Descendant<T> {
@@ -201,6 +293,24 @@ enum Descendant<T> {
 }
 
 impl<T> Descendant<T> {
+    unsafe fn remove(&mut self, node: &LNode<u32, T>) {
+        match self {
+            Descendant::One { ref mut min } => {
+                if ptr::eq(min.as_ref(), node) {
+                    debug_assert!(!node.next.is_null());
+                    *min = ptr::NonNull::new_unchecked(node.next);
+                }
+            }
+            Descendant::Zero { ref mut max } => {
+                if ptr::eq(max.as_ref(), node) {
+                    debug_assert!(!node.prev.is_null());
+                    *max = ptr::NonNull::new_unchecked(node.prev);
+                }
+            }
+            _ => {}
+        }
+    }
+
     fn merge(
         &mut self,
         other: Descendant<T>,
@@ -209,6 +319,7 @@ impl<T> Descendant<T> {
         let old = std::mem::replace(self, Descendant::Both);
         let replacement = match (old, other) {
             (Descendant::Both, _) => Descendant::Both,
+            (_, Descendant::Both) => Descendant::Both,
             (Descendant::Zero { max: mut m }, Descendant::One { .. }) => {
                 unsafe {
                     node.as_mut().prev = m.as_ptr();
@@ -247,7 +358,6 @@ impl<T> Descendant<T> {
                     Descendant::One { min: m1 }
                 }
             }
-            (_, Descendant::Both) => unreachable!(),
         };
         std::mem::replace(self, replacement);
     }
@@ -305,6 +415,8 @@ mod test {
             }
         );
 
+        assert_eq!(lss.root, Some(Descendant::One { min: ptr }));
+
         let mut map = HashMap::new();
         map.insert(
             0b10000000000000000000000000000000,
@@ -335,7 +447,57 @@ mod test {
     }
 
     #[test]
-    fn test_xfast_integration_1() {
+    fn test_xfast_remove_single_element() {
+        let mut lss = LevelSearch::new();
+        let p0 = lss.insert(0xF000_0000, b'a');
+        lss.remove(unsafe { p0.as_ref() });
+
+        assert_eq!(lss.root, None);
+        for map in lss.maps.iter() {
+            assert!(map.is_empty());
+        }
+    }
+
+    #[test]
+    fn test_xfast_remove_root_both() {
+        let mut lss = LevelSearch::new();
+        let p0 = lss.insert(0x0000_0000, b'a');
+        let p1 = lss.insert(0x8000_0000, b'a');
+        assert_eq!(lss.root, Some(Descendant::Both));
+
+        lss.remove(unsafe { p0.as_ref() });
+        assert_eq!(lss.root, Some(Descendant::One { min: p1 }));
+
+        let mut expected = HashMap::new();
+        expected.insert(0x8000_0000, Descendant::Zero { max: p1 });
+        for map in lss.maps.iter() {
+            assert_eq!(map, &expected);
+        }
+
+        lss.remove(unsafe { p1.as_ref() });
+        assert_eq!(lss.root, None);
+        for map in lss.maps.iter() {
+            assert!(map.is_empty());
+        }
+    }
+
+    #[test]
+    fn test_xfast_remove_non_root() {
+        let mut lss = LevelSearch::new();
+        let p0 = lss.insert(0x0000_0000, b'a');
+        let p1 = lss.insert(0x0000_F000, b'a');
+        lss.remove(unsafe { p1.as_ref() });
+
+        assert_eq!(lss.root, Some(Descendant::Zero { max: p0 }));
+        let mut expected = HashMap::new();
+        expected.insert(0x0000_0000, Descendant::Zero { max: p0 });
+        for map in lss.maps.iter() {
+            assert_eq!(map, &expected);
+        }
+    }
+
+    #[test]
+    fn test_xfast_integration_insert() {
         let keys: [u32; 32] = [
             0xcd59c9de, 0x856cb188, 0x6eaaa008, 0xde8db9a9, 0xac3c6ef9,
             0xaba4ba19, 0xc521efbc, 0x866621f3, 0xed3b37a2, 0xda2a7ce7,
@@ -347,23 +509,62 @@ mod test {
         ];
 
         let mut xfast = XFastMap::new();
+        // Empty map, no predecessor
         for key in keys.iter() {
             assert_eq!(xfast.predecessor(*key), None);
         }
 
         for (i, key) in keys.iter().enumerate() {
-            let (i, key) = (i as u32, *key);
-            assert_eq!(xfast.insert(key, 0xdeadbeefu32), None);
-            assert_eq!(xfast.predecessor(key), Some(&0xdeadbeef));
-            assert_eq!(xfast.predecessor(0), None);
+            let (key, value) = (*key, i as u32);
+            assert_eq!(xfast.insert(key, 0), None);
+            // Replace the old key
+            assert_eq!(xfast.insert(key, value), Some(0));
 
-            assert_eq!(xfast.insert(key, i), Some(0xdeadbeef));
-            assert_eq!(xfast.predecessor(key), Some(&i));
-            assert_eq!(xfast.predecessor(0), None);
+            // Predecessor works
+            assert_eq!(xfast.predecessor(key), Some(&value));
 
-            let mut sorted = (0..=i).collect::<Vec<_>>();
+            // Check that predecessor works for each sorted element
+            let mut sorted = (0..=i as u32).collect::<Vec<_>>();
             sorted.sort_unstable_by_key(|k| keys[*k as usize]);
+            for (j, ki) in sorted.iter().enumerate() {
+                let k = keys[*ki as usize];
+                assert_eq!(xfast.predecessor(k), Some(ki));
+                assert_eq!(xfast.predecessor(k + 1), Some(ki));
 
+                if j == 0 {
+                    assert_eq!(xfast.predecessor(k - 1), None);
+                } else {
+                    assert_eq!(xfast.predecessor(k - 1), Some(&sorted[j - 1]));
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_xfast_integration_remove() {
+        let keys: [u32; 32] = [
+            0xcd59c9de, 0x856cb188, 0x6eaaa008, 0xde8db9a9, 0xac3c6ef9,
+            0xaba4ba19, 0xc521efbc, 0x866621f3, 0xed3b37a2, 0xda2a7ce7,
+            0x63df9f0a, 0xb2e4be7c, 0x9c69cb0d, 0x808375c4, 0xbc42de68,
+            0x73f9c015, 0x72903697, 0xb12ad490, 0x9282c1c2, 0x8d4ac30e,
+            0xfb1c49e7, 0x9ffdd800, 0x40fd421f, 0x3aa9e7b1, 0x7a20774e,
+            0xb940e532, 0x749fee0d, 0x0e6c8517, 0x0fa4dc69, 0x205ec45f,
+            0xc8281c71, 0xedd6b0c7,
+        ];
+        let mut xfast = XFastMap::new();
+        for (i, key) in keys.iter().enumerate() {
+            let (key, value) = (*key, i as u32);
+            assert_eq!(xfast.insert(key, value), None);
+        }
+
+        for (i, key) in keys.iter().enumerate() {
+            let (key, value) = (*key, i as u32);
+            assert_eq!(xfast.remove(key), Some(value));
+            assert_eq!(xfast.remove(key), None);
+
+            // Check that predecessor works for each sorted element
+            let mut sorted = (1 + i as u32..32).collect::<Vec<_>>();
+            sorted.sort_unstable_by_key(|k| keys[*k as usize]);
             for (j, ki) in sorted.iter().enumerate() {
                 let k = keys[*ki as usize];
                 assert_eq!(xfast.predecessor(k), Some(ki));
