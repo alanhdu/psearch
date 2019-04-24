24d3c48fd6f8d109f44fd62b91a388f2836b013d
Alan Du <alanhdu@gmail.com>
2019-04-23 22:34:56 -0400

    Add some proptests

--- src/succinct/select_rank.rs
+++ src/succinct/select_rank.rs
@@ -54,7 +54,7 @@ impl Bits256 {
 
     /// Insert a bit at our index
     pub(super) fn insert_bit(&mut self, index: usize, bit: bool) {
-        debug_assert!(index < self.len as usize);
+        debug_assert!(index <= self.len as usize);
 
         let index = index as u8;
         let mut upper = (index >> 6) as usize;
@@ -72,8 +72,8 @@ impl Bits256 {
             .to_le_bytes();
 
         self.len += 1;
+        upper += 1;
         while upper * 64 < self.len as usize {
-            upper += 1;
             let old = last;
             last = self.bits[upper] >> 63;
             self.ones = (u32::from_le_bytes(self.ones)
@@ -81,6 +81,7 @@ impl Bits256 {
                 - (old as u32) * INCREMENT[upper])
                 .to_le_bytes();
             self.bits[upper] = (self.bits[upper] << 1) | (old as u64);
+            upper += 1;
         }
     }
 
@@ -208,28 +209,32 @@ impl Bits256 {
 #[cfg(test)]
 mod test {
     use super::*;
+    use proptest::prelude::*;
 
     #[test]
     fn test_sizes() {
-        assert_eq!(std::mem::size_of::<Bits256>(), 5 * std::mem::size_of::<u64>());
+        assert_eq!(
+            std::mem::size_of::<Bits256>(),
+            5 * std::mem::size_of::<u64>()
+        );
     }
 
     #[test]
-    fn test_leaf_insert_bit() {
-        let mut leaf = Bits256 {
+    fn test_bits256_insert_bit() {
+        let mut bits256 = Bits256 {
             ones: [0, 0, 0, 0],
             len: 63,
             bits: [0, 0, 0, 0],
         };
 
-        leaf.insert_bit(0, true);
-        leaf.insert_bit(25, true);
-        leaf.insert_bit(25, false);
-        leaf.insert_bit(25, true);
-        leaf.insert_bit(64, true);
+        bits256.insert_bit(0, true);
+        bits256.insert_bit(25, true);
+        bits256.insert_bit(25, false);
+        bits256.insert_bit(25, true);
+        bits256.insert_bit(64, true);
 
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 3, 4, 4],
                 len: 68,
@@ -239,16 +244,16 @@ mod test {
     }
 
     #[test]
-    fn test_leaf_remove_bit() {
-        let mut leaf = Bits256 {
+    fn test_bits256_remove_bit() {
+        let mut bits256 = Bits256 {
             ones: [0, 3, 5, 5],
             len: 68,
             bits: [0b1 | 1 << 25 | 1 << 27, 0b11, 0, 0],
         };
 
-        assert_eq!(leaf.remove_bit(1), false);
+        assert_eq!(bits256.remove_bit(1), false);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 4, 5, 5],
                 len: 67,
@@ -256,9 +261,9 @@ mod test {
             }
         );
 
-        assert_eq!(leaf.remove_bit(64), true);
+        assert_eq!(bits256.remove_bit(64), true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 4, 4, 4],
                 len: 66,
@@ -266,9 +271,9 @@ mod test {
             }
         );
 
-        assert_eq!(leaf.remove_bit(63), true);
+        assert_eq!(bits256.remove_bit(63), true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 3, 3, 3],
                 len: 65,
@@ -276,9 +281,9 @@ mod test {
             }
         );
 
-        assert_eq!(leaf.remove_bit(0), true);
+        assert_eq!(bits256.remove_bit(0), true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 2, 2, 2],
                 len: 64,
@@ -288,16 +293,16 @@ mod test {
     }
 
     #[test]
-    fn test_leaf_set_bit() {
-        let mut leaf = Bits256 {
+    fn test_bits256_set_bit() {
+        let mut bits256 = Bits256 {
             ones: [0, 0, 0, 0],
             len: 256,
             bits: [0, 0, 0, 0],
         };
 
-        leaf.set_bit(0, true);
+        bits256.set_bit(0, true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 1, 1, 1],
                 len: 256,
@@ -305,9 +310,9 @@ mod test {
             }
         );
 
-        leaf.set_bit(254, true);
+        bits256.set_bit(254, true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 1, 1, 1],
                 len: 256,
@@ -315,9 +320,9 @@ mod test {
             }
         );
 
-        leaf.set_bit(7, true);
+        bits256.set_bit(7, true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 2, 2, 2],
                 len: 256,
@@ -325,9 +330,9 @@ mod test {
             }
         );
 
-        leaf.set_bit(101, true);
+        bits256.set_bit(101, true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 2, 3, 3],
                 len: 256,
@@ -335,9 +340,9 @@ mod test {
             }
         );
 
-        leaf.set_bit(208, true);
+        bits256.set_bit(208, true);
         assert_eq!(
-            leaf,
+            bits256,
             Bits256 {
                 ones: [0, 2, 3, 3],
                 len: 256,
@@ -346,47 +351,47 @@ mod test {
         );
 
         assert_eq!(
-            (0..5).map(|b| leaf.select1(b)).collect::<Vec<_>>(),
+            (0..5).map(|b| bits256.select1(b)).collect::<Vec<_>>(),
             vec![0, 7, 101, 208, 254]
         );
     }
 
     #[test]
-    fn test_leaf_select_rank_full_zeros() {
-        let leaf = Bits256 {
+    fn test_bits256_select_rank_full_zeros() {
+        let bits256 = Bits256 {
             ones: [0, 0, 0, 0],
             len: 256,
             bits: [0, 0, 0, 0],
         };
 
-        assert_eq!(leaf.num_ones(), 0);
-        assert_eq!(leaf.num_zeros(), 256);
+        assert_eq!(bits256.num_ones(), 0);
+        assert_eq!(bits256.num_zeros(), 256);
         for i in 0..=255 {
-            assert_eq!(leaf.rank1(i), 0);
-            assert_eq!(leaf.rank0(i), i);
-            assert_eq!(leaf.select0(i), i);
+            assert_eq!(bits256.rank1(i), 0);
+            assert_eq!(bits256.rank0(i), i);
+            assert_eq!(bits256.select0(i), i);
         }
     }
 
     #[test]
-    fn test_leaf_select_rank_full_ones() {
-        let leaf = Bits256 {
+    fn test_bits256_select_rank_full_ones() {
+        let bits256 = Bits256 {
             ones: [0, 64, 128, 3 * 64],
             len: 256,
             bits: [u64::max_value(); 4],
         };
-        assert_eq!(leaf.num_ones(), 256);
-        assert_eq!(leaf.num_zeros(), 0);
+        assert_eq!(bits256.num_ones(), 256);
+        assert_eq!(bits256.num_zeros(), 0);
         for i in 0..=255 {
-            assert_eq!(leaf.rank1(i), i);
-            assert_eq!(leaf.rank0(i), 0);
-            assert_eq!(leaf.select1(i), i);
+            assert_eq!(bits256.rank1(i), i);
+            assert_eq!(bits256.rank0(i), 0);
+            assert_eq!(bits256.select1(i), i);
         }
     }
 
     #[test]
-    fn test_leaf_select_rank_half_ones() {
-        let leaf = Bits256 {
+    fn test_bits256_select_rank_half_ones() {
+        let bits256 = Bits256 {
             ones: [0, 32, 64, 80],
             len: 160,
             bits: [
@@ -396,21 +401,75 @@ mod test {
                 0,
             ],
         };
-        assert_eq!(leaf.num_ones(), 80);
-        assert_eq!(leaf.num_zeros(), 80);
+        assert_eq!(bits256.num_ones(), 80);
+        assert_eq!(bits256.num_zeros(), 80);
 
         for i in 0..160 {
-            assert_eq!(leaf.rank0(i), i / 2);
+            assert_eq!(bits256.rank0(i), i / 2);
 
             // For the bits ...01010101, rank1 should go
             //  0, 1, 1, 2, 2, 3, 3, 4, 4, ...
-            // (we are exclusive, so leaf.rank1(0) == 0)
-            assert_eq!(leaf.rank1(i), (i + 1) / 2);
+            // (we are exclusive, so bits256.rank1(0) == 0)
+            assert_eq!(bits256.rank1(i), (i + 1) / 2);
         }
 
         for i in 0..80 {
-            assert_eq!(leaf.select0(i), 2 * i + 1);
-            assert_eq!(leaf.select1(i), 2 * i);
+            assert_eq!(bits256.select0(i), 2 * i + 1);
+            assert_eq!(bits256.select1(i), 2 * i);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_bits256_prop_insert(bits
+            in proptest::collection::vec(proptest::bool::ANY, 1..255)) {
+            let mut bits256 = Bits256 {
+                ones: [0, 0, 0, 0],
+                len: 0,
+                bits: [0, 0, 0, 0],
+            };
+
+            for (i, bit) in bits.iter().cloned().enumerate() {
+                bits256.insert_bit(i, bit);
+            }
+
+            // Test struct values
+            assert_eq!(bits256.len as usize, bits.len());
+            assert_eq!(
+                bits256.ones,
+                [
+                    0,
+                    bits.iter().zip(0..64).map(|(a, _b)| *a as u8).sum(),
+                    bits.iter().zip(0..128).map(|(a, _b)| *a as u8).sum(),
+                    bits.iter().zip(0..192).map(|(a, _b)| *a as u8).sum(),
+                ]
+            );
+
+            assert_eq!(
+                bits256.num_ones(),
+                bits.iter().cloned().filter(|b| *b).count() as u32
+            );
+            assert_eq!(
+                bits256.num_zeros(),
+                bits.iter().cloned().filter(|b| !*b).count() as u32
+            );
+
+            for i in 0..bits256.num_ones() {
+                assert!(bits[bits256.select1(i) as usize]);
+            }
+            for i in 0..bits256.num_zeros() {
+                assert!(!bits[bits256.select0(i) as usize]);
+            }
+
+            let mut c0 = 0;
+            let mut c1 = 0;
+            for (i, bit) in bits.iter().cloned().enumerate() {
+                assert_eq!(bits256.rank0(i as u32), c0);
+                assert_eq!(bits256.rank1(i as u32), c1);
+
+                c0 += !bit as u32;
+                c1 += bit as u32;
+            }
         }
     }
 }
