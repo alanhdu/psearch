c1d6d17a253aa99f46065ca29f79fa7224ef0e90
Alan Du <alanhdu@gmail.com>
2019-05-03 12:23:10 -0400

    Use 510 sized array

--- src/succinct/louds/bytes.rs
+++ src/succinct/louds/bytes.rs
@@ -27,17 +27,17 @@ impl ByteTree {
 }
 
 pub(super) struct ByteLeaf {
-    len: u8,
-    bytes: [u8; 255],
+    len: u16,
+    bytes: [u8; 510],
     next: *mut ByteLeaf,
 }
 
 impl Leaf for ByteLeaf {
     type Output = u8;
-    const CAPACITY: usize = 255;
+    const CAPACITY: usize = 510;
 
     fn is_full(&self) -> bool {
-        self.len == 255
+        self.len == 510
     }
 
     fn len(&self) -> usize {
@@ -51,7 +51,7 @@ impl Leaf for ByteLeaf {
     fn new(value: u8) -> Self {
         let mut leaf = ByteLeaf {
             len: 1,
-            bytes: [0; 255],
+            bytes: [0; 510],
             next: std::ptr::null_mut(),
         };
         leaf.bytes[0] = value;
@@ -61,19 +61,19 @@ impl Leaf for ByteLeaf {
     fn split(&mut self) -> Box<Self> {
         debug_assert!(self.is_full());
 
-        self.len = 127;
+        self.len = 255;
         let mut leaf = Box::new(ByteLeaf {
-            len: 128,
+            len: 255,
             bytes: [0; Self::CAPACITY],
             next: self.next,
         });
-        self.bytes[127..].swap_with_slice(&mut leaf.bytes[..128]);
+        self.bytes[255..].swap_with_slice(&mut leaf.bytes[..255]);
         self.next = Box::into_raw(leaf);
         unsafe { Box::from_raw(self.next) }
     }
 
     fn insert(&mut self, index: usize, byte: u8) {
-        if index < 254 {
+        if index < 510 - 1 {
             unsafe {
                 std::ptr::copy(
                     &self.bytes[index],
@@ -94,6 +94,9 @@ impl ByteLeaf {
         degree: usize,
         needle: u8,
     ) -> (usize, bool) {
+        // This implementation relies on the fact that CAPACITY = 510
+        // Each node is at least 1/2 full, so the next node has at least 255 bytes
+        // Because degree is at most 256, we need to only look at one node ahead
         if index + degree > self.len as usize {
             if self.bytes[self.len as usize - 1] < needle {
                 let next = unsafe { self.next.as_ref() }.unwrap();
@@ -124,8 +127,8 @@ mod test {
     fn test_byteleaf_split() {
         let next = Box::into_raw(Box::new(ByteLeaf::new(0)));
         let mut leaf = ByteLeaf {
-            len: 255,
-            bytes: [0xAA; 255],
+            len: 510,
+            bytes: [0xAA; 510],
             next: next,
         };
         let new = leaf.split();
@@ -133,15 +136,15 @@ mod test {
         let new = unsafe { Box::from_raw(raw) };
 
         assert_eq!(leaf.next, raw);
-        assert_eq!(leaf.len, 127);
-        let mut expected = vec![0xAAu8; 127];
-        expected.append(&mut vec![0; 128]);
+        assert_eq!(leaf.len, 255);
+        let mut expected = vec![0xAAu8; 255];
+        expected.append(&mut vec![0; 255]);
         assert_eq!(&leaf.bytes as &[u8], &expected as &[u8]);
 
-        assert_eq!(new.len, 128);
+        assert_eq!(new.len, 255);
         assert_eq!(new.next, next);
-        let mut expected = vec![0xAAu8; 128];
-        expected.append(&mut vec![0; 127]);
+        let mut expected = vec![0xAAu8; 255];
+        expected.append(&mut vec![0; 255]);
         assert_eq!(&new.bytes as &[u8], &expected as &[u8]);
 
         unsafe { Box::from_raw(next) };
@@ -152,7 +155,8 @@ mod test {
         let mut leaf = ByteLeaf::new(255);
         let mut expected = vec![255u8];
 
-        for i in 0..254 {
+        for i in 0..509 {
+            let i = (i % 256) as u8;
             leaf.insert(0, i);
             expected.insert(0, i);
 
@@ -166,7 +170,8 @@ mod test {
         let mut leaf = ByteLeaf::new(255);
         let mut expected = vec![255u8];
 
-        for i in 0..254 {
+        for i in 0..509 {
+            let i = (i % 256) as u8;
             leaf.insert(leaf.len(), i);
             expected.insert(expected.len(), i);
 
@@ -180,7 +185,8 @@ mod test {
         let mut leaf = ByteLeaf::new(255);
         let mut expected = vec![255u8];
 
-        for i in 0..254 {
+        for i in 0..509 {
+            let i = (i % 256) as u8;
             leaf.insert(leaf.len() / 2, i);
             expected.insert(expected.len() / 2, i);
 
